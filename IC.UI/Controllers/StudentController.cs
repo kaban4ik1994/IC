f3bdede0fc69bdcf509a78ee68546188ad16a4f6
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-06-01 22:26:25 +0300

    v 0.2.1

---
+++ IC.UI/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using IC.UI.Filters.AuthorizationFilters;
+
+namespace IC.UI.Controllers
+{
+
+    public class StudentController : Controller
+    {
+        [CheckRole("Admin,User")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ IC.UI/Models/StudentViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using IC.Entities.Models;
+
+namespace IC.UI.Models
+{
+    public class StudentViewModel
+    {
+        public IEnumerable<int> Courses { get; set; }
+        public IEnumerable<string> Specialties { get; set; }
+        public IEnumerable<int> Groups { get; set; }
+        public IEnumerable<int> YearOfEntrances { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
