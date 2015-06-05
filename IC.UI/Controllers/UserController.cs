46699d229259f62119b5d59d5b798ddcea04fd6c
kaban4ik1994 <kaban4ik19901990@mail.ru>
2015-06-05 19:01:02 +0300

    v 1.7

---
+++ IC.UI/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using IC.Services.Interfaces;
+using IC.UI.Filters.AuthorizationFilters;
+using IC.UI.Models;
+using Repository.Pattern.UnitOfWork;
+
+namespace IC.UI.Controllers
+{
+    [CheckRole("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUserService _userService;
+        private readonly IUnitOfWorkAsync _unitOfWork;
+
+        public UserController(IUserService userService, IUnitOfWorkAsync unitOfWork)
+        {
+            _userService = userService;
+            _unitOfWork = unitOfWork;
+        }
+
+        public ActionResult Index()
+        {
+            return View(_userService.Query().Select(user => new UserViewModel
+            {
+                Email = user.Email,
+                FirstName = user.FirstName,
+                SecondName = user.SecondName,
+                UserId = user.UserId,
+                IsAdmin = user.UserRoles.Where(role => role.Role.Name == "Admin").Count() != 0,
+                IsUser = user.UserRoles.Where(role => role.Role.Name == "User").Count() != 0,
+            }).ToList());
+        }
+    }
+}
---
+++ IC.UI/Models/UserViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace IC.UI.Models
+{
+    public class UserViewModel
+    {
+        public long UserId { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsUser { get; set; }
+    }
+}
